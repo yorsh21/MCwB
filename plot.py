dbae77027c6a03b64f3da532ccb61b36b7119e62
Jorge Villagran <bkn_21@hotmail.com>
2018-11-08 21:19:32 -0800

    Plot milk routes.

--- plot.py
+++ plot.py
@@ -1,40 +1,74 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import pygraphviz as pgv
-import math
+import sys
+import numpy as np
+from pylab import *
+import matplotlib.pyplot as plt
 
-#Diccionario de nodos y arcos
+def read_instances(instance):
+	file = open("inputs/" + instance)
 
-G = pgv.AGraph()
-G.node_attr.update(color='red')
-G.edge_attr.update(len='10.0',color='blue')
+	num_trucks = int(file.readline())
+	trucks_capacities = file.readline()[:-1].split("\t")
+	file.readline()
+	num_milks = int(file.readline())
+	milk_types = file.readline()[:-1].split("\t")
+	milk_values = file.readline()[:-1].split("\t")
+	file.readline()
+	num_farms = int(file.readline())
 
-x = [15, 1, 87, 69, 93, 33, 71, 29, 93, 55, 23, 19, 57, 5, 65, 69, 3, 19, 21, 67, 41, 19, 15, 79, 19, 27, 29, 25, 59, 27, 21, 61, 15, 31, 71, 91]
-y = [19, 49, 25, 65, 91, 31, 61, 9, 7, 47, 13, 47, 63, 95, 43, 1, 25, 91, 81, 91, 23, 75, 79, 47, 65, 49, 17, 65, 51, 95, 91, 83, 83, 87, 41, 21]
-data = [0, 34, 15, 26, 24, 7, 2, 0, 8, 21, 6, 30, 33, 20, 5, 17, 31, 12, 35, 1, 16, 4, 0, 13, 32, 19, 27, 3, 22, 29, 23, 14, 11, 28, 18, 10, 25, 9, 0]
+	farms = list()
+	for farm in range(num_farms):
+	    farms.append(file.readline()[:-1].split("\t"))
 
-for i in range(len(data)):
-	for j in range(i, len(data)):
-		if i != j:
-			distance = int(math.sqrt((x[data[i]]-x[data[j]])**2 + (y[data[i]]-y[data[j]])**2))
-			G.add_edge(str(data[i]), str(data[j]),label=str(distance))
+	return farms
 
-'''
-G.add_edge('a','b',label='8')
-G.add_edge('a','c',label='2')
-G.add_edge('a','e',label='6')
-G.add_edge('a','d',label='4')
-G.add_edge('b','c',label='6')
-G.add_edge('b','e',label='9')
-G.add_edge('b','d',label='12')
-G.add_edge('c','e',lebel='5')
-G.add_edge('c','d',label='3')
-G.add_edge('d','e',label='4')
-'''
 
-#print(G)
+def plot_map(route = None):
+	colors = ['c', 'm', 'y']
+	line = '-'
+	plt.figure(num=None, figsize=(14, 10), dpi=100, facecolor='w', edgecolor='k')
+	plt.title('Draw Milk Routes')
+	plt.xlabel('x')
+	plt.ylabel('y')
 
-G.layout()
+	
+	for i in range(len(farms)-1):
+	    if farms[i][3] == "-":
+	        scatter(int(farms[i][1]), int(farms[i][2]), s=80 ,marker='s', c='k')
+	    elif farms[i][3] == "A":
+	        scatter(int(farms[i][1]), int(farms[i][2]), s=30 ,marker='o', c='r')
+	    elif farms[i][3] == "B":
+	        scatter(int(farms[i][1]), int(farms[i][2]), s=30 ,marker='o', c='g')
+	    elif farms[i][3] == "C":
+	        scatter(int(farms[i][1]),int(farms[i][2]), s=30 ,marker='o', c='b')
+	        
+	if route is not None:
+		count_route = -1
+		for i in range(len(route)-1):
+			if route[i] == 0:
+				count_route += 1
 
-G.draw('file.png')
+			plot([int(farms[route[i]][1]), int(farms[route[i+1]][1])],
+				[int(farms[route[i]][2]), int(farms[route[i+1]][2])], colors[count_route]+line, linewidth = .5)
+
+	#plt.grid()
+	plt.show()
+
+
+
+#Read Args
+if len(sys.argv) == 1:
+	print("It is need pass the instance name")
+elif len(sys.argv) == 2:
+	farms = read_instances(sys.argv[1])
+	plot_map()
+elif len(sys.argv) == 3:
+	farms = read_instances(sys.argv[1])
+	route = list(map(int, sys.argv[2][1:-1].split(",")))
+	print(route)
+	plot_map(route)
+
+
+#route = [0, 34, 15, 26, 24, 7, 2, 0, 8, 21, 6, 30, 33, 20, 5, 17, 31, 12, 35, 1, 16, 4, 0, 13, 32, 19, 27, 3, 22, 29, 23, 14, 11, 28, 18, 10, 25, 9, 0]
